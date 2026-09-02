e21198971efc2318dc80b3e5ab68a2e09b195fe9
DAS-EASH <das.eashwar71@gmail.com>
2026-09-02 16:47:36 +0530

    Time: 15 ms (46.42%), Space: 23.8 MB (76.21%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     string getSmallestString(int n, int k) {
       
-    string ans;
-    while (n != 0) {
-        int ch = k - (n - 1) * 26;
-        if (ch <= 0) {
-            ans.push_back('a');
-            k--;
-            n--;
-        } else {
-            ans.push_back(ch + 'a' - 1);
-            n--;
-            k -= (ch);
-        }
+
+    string ans(n, 'a');
+    k -= n; // Each 'a' already accounts for 1
+
+    for (int i = n - 1; i >= 0 && k > 0; i--) {
+        int add = min(25, k);
+        ans[i] += add;
+        k -= add;
     }
+
     return ans;
+
 }
   
 };
